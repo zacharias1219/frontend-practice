edda0b56ddf84d2f8842b1c55f524af920b19461
Richard Abishai <rickoshade1891@gmail.com>
2025-01-31 14:09:40 +0530

    feat: restructure App component and add Footer, Main, and SideBar components; update index.html title and include Font Awesome

---
+++ nasa/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div>
+        <h2>The Brutal Martial Landscape</h2>
+        <h1>APOD PROJECT</h1>
+    </div>
+  )
+}
