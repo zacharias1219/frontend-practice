54124df914ae9961a7834f584d69db8a3c52b8cb
Richard Abishai <rickoshade1891@gmail.com>
2025-02-06 11:51:34 +0530

    feat: add Header component to Generator and refactor SectionWrap; introduce Button component

--- gym-app/src/components/Generator.jsx
+++ gym-app/src/components/Generator.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import SectionWrap from './SectionWrap'
 
+function Header(props) {
+  const { index, title, description } = props
+  return (
+      <div className='flex flex-col gap-4'>
+          <div className='flex items-center justify-center gap-2'>
+              <p className='text-3xl sm:text-4xl md:text-5xl font-semibold text-slate-400'>{index}</p>
+              <h4 className='text-xl sm:text-2xl md:text-3xl'>{title}</h4>
+          </div>
+          <p className='text-sm sm:text-base mx-auto'>{description}</p>
+      </div>
+  )
+}
+
 export default function Generator() {
   return (
     <SectionWrap header = {"Generate your Workout"} title={['It\'s', 'Workout', 'Time!']}>
+      <Header index={'01'} title={'Pick your poison'} description={"Select the workout you wish to endure."} />
       
     </SectionWrap>
   )
