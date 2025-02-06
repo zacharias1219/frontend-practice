38092d74913b75583da9941454b1efc75d97bf26
Richard Abishai <rickoshade1891@gmail.com>
2025-02-06 11:54:39 +0530

    feat: integrate Button component into Hero and add ExerciseCard component for exercise details

--- gym-app/src/components/Hero.jsx
+++ gym-app/src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-// import Button from './Button'
+import Button from './Button'
 
 export default function Hero() {
     return (
@@ -10,9 +10,9 @@ export default function Hero() {
                 <h1 className='uppercase font-semibold text-4xl sm:text-5xl md:text-6xl lg:text-7xl'>Swole<span className='text-blue-400'>normous</span></h1>
             </div>
             <p className='text-sm md:text-base font-light'>I hereby acknowledgement that I may become <span className='text-blue-400 font-medium'>unbelievably swolenormous</span> and accept all risks of becoming the local <span className='text-blue-400 font-medium'>mass montrosity</span>, afflicted with severe body dismorphia, unable to fit through doors.</p>
-            {/* <Button func={() => {
+            <Button func={() => {
                 window.location.href = '#generate'
-            }} text={"Accept & Begin"}></Button> */}
+            }} text={"Accept & Begin"}></Button>
         </div>
     )
 }
